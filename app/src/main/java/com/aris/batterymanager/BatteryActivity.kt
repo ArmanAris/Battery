d83c8014461d9214c018cec8bbe6318a31ad7cc2
ArmanAris <114592462+ArmanAris@users.noreply.github.com>
2022-11-14 09:56:50 +0330

    master: add state battery in page 2

--- app/src/main/java/com/aris/batterymanager/BatteryActivity.kt
+++ app/src/main/java/com/aris/batterymanager/BatteryActivity.kt
@@ -7,11 +7,20 @@ import android.content.IntentFilter
 import android.os.BatteryManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aris.batterymanager.databinding.ActivityBatteryBinding
+import com.aris.batterymanager.databinding.ActivityMainBinding
 
 class BatteryActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityBatteryBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_battery)
+
+        binding = ActivityBatteryBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
 
         registerReceiver(batteryInfo, IntentFilter(Intent.ACTION_BATTERY_CHANGED))
 
@@ -20,10 +29,17 @@ class BatteryActivity : AppCompatActivity() {
     private var batteryInfo: BroadcastReceiver = object : BroadcastReceiver() {
         override fun onReceive(context: Context, intent: Intent) {
             intent.getIntExtra(BatteryManager.EXTRA_LEVEL, 0)
-            intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0)
-            intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10
-            intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000
-            intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY)
+
+            if (intent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) == 0){
+                binding.plugNum.text ="متصل نیست"
+            }else{
+                binding.plugNum.text ="متصل است"
+            }
+            binding.tempNum.text =
+                (intent.getIntExtra(BatteryManager.EXTRA_TEMPERATURE, 0) / 10).toString() + " C°"
+            binding.voltageNum.text =
+                (intent.getIntExtra(BatteryManager.EXTRA_VOLTAGE, 0) / 1000).toString() + " Volt"
+            binding.tecNum.text = intent.getStringExtra(BatteryManager.EXTRA_TECHNOLOGY)
         }
 
     }
